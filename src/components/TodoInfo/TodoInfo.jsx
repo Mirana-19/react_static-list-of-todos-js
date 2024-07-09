105486d73269612f12aeb9955e9be6feaf1d9e6e
Marta Żak <martysik19@gmail.com>
2024-07-09 22:03:14 +0200

    adds todo list

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,13 @@
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => {
+  const completedClassname = todo.completed ? 'TodoInfo--completed' : '';
+
+  return (
+    <li className={`TodoInfo ${completedClassname}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={todo.user} />
+    </li>
+  );
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,14 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { todos } from '../../App';
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = () => {
+  return (
+    todos && (
+      <ul className="TodoList">
+        {todos.map(todo => {
+          return <TodoInfo key={todo.id} todo={todo} />;
+        })}
+      </ul>
+    )
+  );
+};
